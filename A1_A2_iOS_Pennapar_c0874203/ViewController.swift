bf8d274512088b2163f66d9e1ffb5ae65d4c76a5
Alice <c0874203@mylambton.ca>
2023-01-20 16:54:03 -0500

    Initial Commit

---
+++ A1_A2_iOS_Pennapar_c0874203/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_Pennapar_c0874203
+//
+//  Created by Alice’z Poy on 2023-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
